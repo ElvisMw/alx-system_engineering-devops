e3e4db88c5d922a38a6f6aba182171e4fa1aede4
ElvisMw <elvismwanthi@gmail.com~cler>
2023-11-24 20:12:01 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/puppet
-# 1-install_a_package.pp
-package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+  require => Package['python3-pip'],
 }
 
