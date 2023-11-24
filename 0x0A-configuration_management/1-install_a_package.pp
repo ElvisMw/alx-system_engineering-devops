47041ef219a1eb8b2a2ada51fa1cb627d055ead0
ElvisMw <elvismwanthi@gmail.com~cler>
2023-11-24 19:32:29 +0300

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # 1-install_a_package.pp
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure  => '2.1.0',
+  provider  => 'pip3',
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,7 +2,7 @@
 # 2-execute_a_command.pp
 
 exec { 'killmenow':
-command => 'pkill -f killmenow',
-onlyif  => 'pgrep -f killmenow',
-path    => '/usr/bin:/bin', # Specify the path to the pkill and pgrep commands
+  command  => 'pkill -f killmenow',
+  onlyif  => 'pgrep -f killmenow',
+  path  => '/usr/bin:/bin', # Specify the path to the pkill and pgrep commands
 }
