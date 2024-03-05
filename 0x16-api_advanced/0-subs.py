5104492788456e3826fd3c40c725ebf417fa3565
ElvisMw <elvismwanthi@gmail.com>
2024-03-05 19:29:44 +0300

    0-subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API and returns the number
+    of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+
+    response = requests.get(url, headers=headers)
+
+    """ Check if the request was successful (status code 200)"""
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        """ If the subreddit is invalid or any other error occurs, return 0"""
+        return 0
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
