8dc9414f468526c295795a58e78d1cac771dd696
ElvisMw <elvismwanthi@gmail.com>
2024-03-05 20:09:52 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python3
 """
-Script show number of subscribers for Reddit subreddit.
+0-subs
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Shows number of subscribers for subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Function that queries the Reddit API and returns the number
+    of subscribers for a given subreddit.
+    If not a valid subreddit, the function should return 0.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+
+    response = requests.get(url, headers=headers)
+
+    """ Check if the request was successful (status code 200)"""
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+
+        """Check if 'data' and 'subscribers' keys exist in the response"""
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            """If the structure of the response is unexpected, return 0"""
+            return 0
     else:
+        """If the subreddit is invalid or any other error occurs, return 0"""
         return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
