13bc981c9ed86beb930d5ab77189f9b1b4c56473
ElvisMw <elvismwanthi@gmail.com>
2024-03-07 18:13:57 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,21 +6,7 @@
 # to resolve the underlying problem causing the error.
 
 # Define an exec resource to update the Apache configuration
-exec { 'fix-apache-configuration':
-  command     => 'sed -i "s/old_value/new_value/g" /etc/apache2/apache2.conf',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-}
-
-# Notify Apache service to restart when the configuration is updated
-service { 'apache2':
-  ensure    => 'running',
-  subscribe => Exec['fix-apache-configuration'],
-}
-
-# Define an exec resource to reload Apache to apply the changes
-exec { 'reload-apache':
-  command     => '/usr/sbin/service apache2 reload',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
