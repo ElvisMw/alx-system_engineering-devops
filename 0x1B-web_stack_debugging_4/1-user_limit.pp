15c4ae8912a260c61b044828624c482cd441ddd1
ElvisMw <elvismwanthi@gmail.com>
2024-03-16 22:40:01 +0300

    1-user_limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # Puppet manifest to increase file descriptor limit for the holberton user
 
-# Execute a command to increase file descriptor limit for the holberton user
+# Execute a command to increase hard file descriptor limit for the holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increasing file limit, userHolberton
+Execute a command to increase soft file descriptor limit for the holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
