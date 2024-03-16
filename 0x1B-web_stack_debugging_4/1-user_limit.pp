8e82c816f5617df8191a8320e7b31c4576862501
ElvisMw <elvismwanthi@gmail.com>
2024-03-16 22:46:21 +0300

    1-user_limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -6,7 +6,7 @@ exec { 'increase-hard-file-limit-for-holberton-user':
   path    => '/usr/local/bin/:/bin/'
 }
 
-Execute a command to increase soft file descriptor limit for the holberton user
+Execute a command to increase soft file descriptor limit for the holberton user.
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
